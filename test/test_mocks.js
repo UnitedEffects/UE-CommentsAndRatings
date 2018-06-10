398fa73bfc207b76c29965b515958492ce4d056d
Bo Motlagh <bo.motlagh@gmail.com>
2018-06-10 02:06:12 -0400

    test cleanup

--- test/test_mocks.js
+++ test/test_mocks.js
@@ -1,6 +1,30 @@
+const commentTestId1 = '5b09afdd523e6e1957c83c74';
+
 export default {
+    superUser: {
+        _id: '1b09b06b523e6e1957c83c6e',
+        role: 1,
+        mock: true
+    },
+    standardUser: {
+        _id: '2b09b06b523e6e1957c83c6e',
+        role: 0,
+        mock: true
+    },
+    testAccessToken: Buffer.from('1b09b06b523e6e1957c83c6e.abc123abc123.test.test').toString('base64'),
+    testToken (user) {
+        return [{
+            value: 'abc123abc123',
+            user_id: '1b09b06b523e6e1957c83c6e',
+            product_slug:'test',
+            domain_slug: 'test',
+            user: user,
+            created: Date.now()
+        }];
+    },
+    singleCommentTestId: commentTestId1,
     singleComment: {
-        "_id": "5b09afdd523e6e1957c83c74",
+        "_id": commentTestId1,
         "comment": "This is a comment",
         "domain": "test",
         "creator": "1b09b06b523e6e1957c83c6e",
